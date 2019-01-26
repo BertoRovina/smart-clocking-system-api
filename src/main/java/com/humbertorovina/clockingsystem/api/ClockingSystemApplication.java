fab7bb3a3cc5aa8666622ddaa2c1e8c59c337103
Humberto Rovina <32313091+BertoRovina@users.noreply.github.com>
2019-01-26 17:19:16 -0500

    Inicial Commit - Project Files

---
+++ src/main/java/com/humbertorovina/clockingsystem/api/ClockingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.humbertorovina.clockingsystem.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockingSystemApplication.class, args);
+	}
+
+}
+
